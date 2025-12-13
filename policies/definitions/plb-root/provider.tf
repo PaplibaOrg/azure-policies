4edb6070d619a926ed103bde70258fc96cca0946
sunny bharne <sunny.bharne@outlook.com>
2025-12-13 17:41:29 +0200

    testing terraform porvider

---
+++ policies/definitions/plb-root/provider.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~>2.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-tf-state-eus-dev-001"
+    storage_account_name = "sttfstateeusdev001"
+    container_name       = "tfstate"
+    key                  = "policy.definitions.dev.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+  tenant_id = "99e184df-412c-45ed-b033-63f70449fe62"
+}
+
+provider "azapi" {
+  tenant_id = "99e184df-412c-45ed-b033-63f70449fe62"
+}
+
+provider "random" {
+}
